a02ed12b35c8aeb57c7821786aebabf3b7a1ae95
@Computacao_eletrica <97069062+computacaoeletrica@users.noreply.github.com>
2022-01-03 20:32:47 -0400

    Create Vai_um_bhaskara_ai.c
    Um programa que acha as raízes de uma equação do 2° Grau recebendo do usuário o valor dos coeficientes.

---
+++ Vai_um_bhaskara_ai.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  
+  float a,b,c,delta,x1,x2;
+  printf ("Informe o valor do a : ");
+  scanf("%f",&a);
+  printf ("\nInforme o valor do b : ");
+  scanf("%f",&b);
+  printf ("\nInforme o valor do c : ");
+  scanf("%f",&c);
+  delta = ((pow(b,2))-(4*a*c));
+  
+if (delta < 0 )
+{
+  printf ("\nEssa equacao nao possui solucoes reais");
+}
+if (delta == 0)
+{
+  x1= ((-b+sqrt(delta))/2*a);
+  printf ("\n x1 : %.2f",x1);
+}
+if (delta > 0)
+{
+  x1= ((-b+sqrt(delta))/2*a);
+  x2= ((-b-sqrt(delta))/2*a);
+  printf ("\n x1 : %.2f",x1);
+  printf ("\n x2 : %.2f\n\n",x2);
+}
+  
+printf("PROGRAMA ENCERRADO ! ");
+  
+}
